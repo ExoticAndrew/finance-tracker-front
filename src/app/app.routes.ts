3d349dd3284eaedab4a83314c8ff6300ce2a2a3d
ExoticAndrew <andrewantunes61@gmail.com>
2026-04-19 21:42:39 -0300

    feat: projeto inicial angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './features/dashboard/dashboard';
+import { Landing } from './features/landing/landing';
+import { Transacoes } from './features/transacoes/transacoes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Landing },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'transacoes', component: Transacoes },
+  { path: '**', redirectTo: '' }
+];
