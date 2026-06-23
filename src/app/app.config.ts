d9ab61a4c8e9af07719cc0ea05440f0b57b07e53
ExoticAndrew <andrewantunes61@gmail.com>
2026-06-23 02:00:47 -0300

    refactor(core): renomeia interceptions para interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, LOCALE_ID } from
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
-import { authInterceptor } from './core/interceptions/auth.interceptor';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
 
